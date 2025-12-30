ec38b1e92ddc5759c97b60e6e998ff401a20713a
Clam17v <dc15888814636@gmail.com>
2025-12-30 14:45:40 +0800

    one

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -3,13 +3,10 @@ import { Language } from '../../types';
 export const NAV_ITEMS: Record<Language, { id: string; label: string }[]> = {
   zh: [
     { id: 'dashboard', label: '主页' },
-    { id: 'portfolio', label: '作品' }
+    { id: 'portfolio', label: '作品' },
   ],
   en: [
     { id: 'dashboard', label: 'Home' },
     { id: 'portfolio', label: 'Portfolio' },
-    { id: 'articles', label: 'Articles' },
-    { id: 'about', label: 'Education' },
-    { id: 'contact', label: 'Contact' }
   ]
 };
