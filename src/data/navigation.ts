b98ce9806d8be160ca17b8cc52551576403562b6
Clam17v <dc15888814636@gmail.com>
2025-12-30 16:32:49 +0800

    one

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -2,7 +2,7 @@ import { Language } from '../../types';
 
 export const NAV_ITEMS = {
   zh: [
-    { id: 'dashboard', label: '主页123' },
+    { id: 'dashboard', label: '主页' },
     { id: 'portfolio', label: '作品' }
   ],
   en: [
