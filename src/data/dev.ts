fdd5810d08cde63c51461a5e072dfaa9f64dcca2
Clam17v <dc15888814636@gmail.com>
2026-01-04 14:43:56 +0800

    one

--- src/data/dev.ts
+++ src/data/dev.ts
@@ -1,5 +1,23 @@
 import { Project } from '../../types';
 
 export const DEV_DATA: Project[] = [
-  
+  {
+    id: 'dev1',
+    common: {
+      category: 'Videography',
+      image: '/qiche.png', 
+      modalImage: '/qiche1.png',
+      bilibiliId: 'BV1J5S7B6ECM'
+    },
+    zh: {
+      title: '路特斯Emira短片',
+      subtitle: '视频制作',
+      description: '',
+      role: '视频制作',
+      tags: ['毕业晚会', '串场视频'],
+      awards: [],
+      concept: "",
+      roleDetail: ""
+    },
+  },
 ];
